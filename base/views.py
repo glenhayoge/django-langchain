09de49f92d8ee7ec4be545196420b9f77769ff89
Glen Hayoge <glenhayoge@gmail.com>
2024-03-19 08:35:41 +1000

    updated view

--- base/views.py
+++ base/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 def index(request):
     if request.method == 'POST':
         query = request.POST.get('query')
-        result = {}  # Here, we will later get results from embeddings
+        result = {}  # Here, we will get results from embeddings
         return JsonResponse(result)
+    # Here, we will create a database if we don't have one as the user loads onto the page
     return render(request, 'base/index.html')
